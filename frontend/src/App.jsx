300a9a59f24d47092fdb0c84a27d6c931bde31ad
priyansudash1999 <1999priyansudash@gmail.com>
2025-11-09 16:10:14 +0530

    feat(header): design and implement responsive navigation header for frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,24 @@
 import React from 'react'
+import {Route, Routes} from "react-router-dom"
+import { About, Appointments, Contact, Doctors, Home, Login, MyAppointment, Profile } from './pages'
+import { Navbar } from './components'
 
 const App = () => {
   return (
-    <div>App</div>
+    <div className='mx-4 sm:mx-[10%]'>
+      <Navbar />
+      <Routes>
+        <Route path='/' element={<Home />}/>
+        <Route path='/doctors' element={<Doctors />}/>
+        <Route path='/doctors/:speciality' element={<Doctors />}/>
+        <Route path='/contact' element={<Contact />}/>
+        <Route path='/login' element={<Login />}/>
+        <Route path='/about' element={<About />}/>
+        <Route path='/my-profile' element={<Profile />}/>
+        <Route path='/my-appointments' element={<MyAppointment />}/>
+        <Route path='/appointments/:docId' element={<Appointments />}/>
+      </Routes>
+    </div>
   )
 }
 
---
+++ frontend/src/pages/index.js
@@ -0,0 +1,10 @@
+import Home from "./Home";
+import Doctors from "./Doctors";
+import Contact from "./Contact";
+import About from "./About";
+import Login from "./Login";
+import Profile from "./Profile";
+import MyAppointment from "./MyAppointment";
+import Appointments from "./Appointments";
+
+export {Home, Doctors, Contact, About, Login, Profile, MyAppointment, Appointments}
