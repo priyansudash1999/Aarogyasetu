7664b3af2cb6b604f1f666622bccafe586c8c225
priyansudash1999 <1999priyansudash@gmail.com>
2025-11-13 22:23:58 +0530

    feat(doctor): add doctor page with types of doctor only

--- frontend/src/pages/Doctors.jsx
+++ frontend/src/pages/Doctors.jsx
@@ -1,8 +1,27 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { AppContext } from '../context/AppContext'
 
 const Doctors = () => {
+
+  const {doctors} = useContext(AppContext)
+
   return (
-    <div>Doctors</div>
+    <div>
+      <p>Browse through the doctors speciality</p>
+      <div>
+        <div>
+          <p>General Physician</p>
+          <p>Gynecologist</p>
+          <p>Dermatologist</p>
+          <p>Pediatricians</p>
+          <p>Nuerolosgist</p>
+          <p>Gastroenterologist</p>
+        </div>
+        <div>
+          
+        </div>
+      </div>
+    </div>
   )
 }
 
