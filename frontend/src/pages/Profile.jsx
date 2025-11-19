a1561323d80c3ced10a39e3ff2fca50f2e32fcb3
priyansudash1999 <1999priyansudash@gmail.com>
2025-11-19 23:42:46 +0530

    feat: Initiate profile page

--- frontend/src/pages/Profile.jsx
+++ frontend/src/pages/Profile.jsx
@@ -1,8 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'
+import {assets} from "../assets/assets"
 
 const Profile = () => {
+
+  const [data, setData] = useState({
+    name: "Jack Sparrow",
+    image: assets.profile_pic,
+    email: "jacksparrow01@gmail.com",
+    phone: "+19876543210",
+    address: {
+      line1: "65th Cross, Richmond",
+      line2: "Circle, Church Road, London"
+    },
+    gender: 'Male',
+    dob: '1996-10-11'
+  })
+
   return (
-    <div>Profile</div>
+    <div>
+      <img src={data.image} alt="" />
+    </div>
   )
 }
 
